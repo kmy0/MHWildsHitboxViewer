1e2c5ba3475df60e77e25d68ca3f4c2f702ab185
kmy0 <93156259+kmy0@users.noreply.github.com>
2025-05-07 16:46:09 +0200

    move col load

--- src/HitboxViewer/box/hurt/box_queue.lua
+++ src/HitboxViewer/box/hurt/box_queue.lua
@@ -2,13 +2,10 @@
 ---@field queue HurtBoxLoadData[]
 ---@field enqueue fun(self: HurtBoxLoadQueue , load_data: HurtBoxLoadData)
 
----@class (exact) HurtBoxLoadData : QueueDataBase
----@field char Character
----@field col via.physics.Collidable
+---@class (exact) HurtBoxLoadData : HurtBoxColLoadData
 ---@field resource_idx integer
 ---@field set_idx integer
 ---@field collidable_idx integer
----@field userdata via.physics.UserData
 
 local config = require("HitboxViewer.config")
 local data = require("HitboxViewer.data")
--- src/HitboxViewer/box/hurt/init.lua
+++ src/HitboxViewer/box/hurt/init.lua
@@ -19,34 +19,17 @@ local this = {
 
 ---@param char Character
 ---@param rsc via.physics.RequestSetCollider
----@return fun(): via.physics.Collidable, via.physics.UserData, integer, integer, integer
+---@return fun(): integer, integer, integer
 local function get_collidable(char, rsc)
     return coroutine.wrap(function()
         if char.type == rt.enum.char.Player or char.type == rt.enum.char.MasterPlayer then
-            for j = 1, 2 do
-                local col = rsc:getCollidableFromIndex(0, j, 0)
-                if col then
-                    coroutine.yield(col, col:get_UserData(), 0, j, 0)
-                end
-            end
+            coroutine.yield(0, 1, 0)
+            coroutine.yield(0, 2, 0)
         else
             for i = 0, rsc:get_NumRequestSets() - 1 do
                 for j = 0, rsc:getNumRequestSetsFromIndex(i) - 1 do
                     for k = 0, rsc:getNumCollidablesFromIndex(i, j) - 1 do
-                        local col = rsc:getCollidableFromIndex(i, j, k)
-                        if not col then
-                            goto continue
-                        end
-
-                        local userdata = col:get_UserData()
-                        ---@cast userdata via.physics.RequestSetColliderUserData
-                        local p_data = userdata:get_ParentUserData()
-                        if not p_data then
-                            goto continue
-                        end
-
-                        coroutine.yield(col, p_data, i, j, k)
-                        ::continue::
+                        coroutine.yield(i, j, k)
                     end
                 end
             end
@@ -60,40 +43,14 @@ end
 
 function this.get()
     for load_data in col_queue:get() do
-        local char = load_data.char
-
-        for col, userdata, resource_idx, set_idx, collidable_idx in get_collidable(char, load_data.rsc) do
-            local data_type = userdata:get_type_definition() --[[@as RETypeDefinition]]
-            local box_data = {
-                char = char,
-                col = col,
+        for resource_idx, set_idx, collidable_idx in get_collidable(load_data.char, load_data.rsc) do
+            box_queue:enqueue({
+                char = load_data.char,
+                rsc = load_data.rsc,
                 resource_idx = resource_idx,
                 set_idx = set_idx,
                 collidable_idx = collidable_idx,
-                userdata = userdata,
-            }
-
-            -- stylua: ignore start
-            if
-                char.type == rt.enum.char.Npc
-                and (
-                    data_type:is_a("app.col_user_data.DamageParamNpc")
-                    ---@diagnostic disable-next-line: cast-type-mismatch
-                    ---@cast data_type app.col_user_data.DamageParamNpc
-                    and data_type._IsAttackDetector
-                )
-            -- stylua: ignore end
-            then
-                box_queue:enqueue(box_data)
-            elseif char.type == rt.enum.char.Player or char.type == rt.enum.char.MasterPlayer then
-                box_queue:enqueue(box_data)
-            elseif char.type == rt.enum.char.Pet and data_type:is_a("app.col_user_data.DamageParamOt") then
-                box_queue:enqueue(box_data)
-            elseif char.type == rt.enum.char.BigMonster and data_type:is_a("app.col_user_data.DamageParamEm") then
-                box_queue:enqueue(box_data)
-            elseif char.type == rt.enum.char.SmallMonster and data_type:is_a("app.col_user_data.DamageParamEm") then
-                box_queue:enqueue(box_data)
-            end
+            })
         end
     end
 
@@ -101,48 +58,61 @@ function this.get()
         ---@type HurtBoxBase?
         local box
         local char = load_data.char
+        local col =
+            load_data.rsc:getCollidableFromIndex(load_data.resource_idx, load_data.set_idx, load_data.collidable_idx)
 
-        if char.type == rt.enum.char.Npc then
+        if not col then
+            goto continue
+        end
+
+        local userdata = col:get_UserData()
+        ---@cast userdata via.physics.RequestSetColliderUserData
+        local p_data = userdata:get_ParentUserData()
+
+        if not p_data then
+            goto continue
+        end
+
+        local data_type = p_data:get_type_definition() --[[@as RETypeDefinition]]
+
+        -- stylua: ignore start
+        if
+            char.type == rt.enum.char.Npc
+            and (
+                data_type:is_a("app.col_user_data.DamageParamNpc")
+                ---@diagnostic disable-next-line: cast-type-mismatch
+                ---@cast p_data app.col_user_data.DamageParamNpc
+                and p_data._IsAttackDetector
+            )
+        -- stylua: ignore end
+        then
             ---@cast char Npc
-            box =
-                friend.npc:new(load_data.col, char, load_data.resource_idx, load_data.set_idx, load_data.collidable_idx)
+            box = friend.npc:new(col, char, load_data.resource_idx, load_data.set_idx, load_data.collidable_idx)
         elseif char.type == rt.enum.char.Player or char.type == rt.enum.char.MasterPlayer then
             ---@cast char Player
-            box = friend.player:new(
-                load_data.col,
-                char,
-                load_data.resource_idx,
-                load_data.set_idx,
-                load_data.collidable_idx
-            )
-        elseif char.type == rt.enum.char.Pet then
+            box = friend.player:new(col, char, load_data.resource_idx, load_data.set_idx, load_data.collidable_idx)
+        elseif char.type == rt.enum.char.Pet and data_type:is_a("app.col_user_data.DamageParamOt") then
             ---@cast char Pet
-            box =
-                friend.pet:new(load_data.col, char, load_data.resource_idx, load_data.set_idx, load_data.collidable_idx)
-        elseif char.type == rt.enum.char.BigMonster then
+            box = friend.pet:new(col, char, load_data.resource_idx, load_data.set_idx, load_data.collidable_idx)
+        elseif char.type == rt.enum.char.BigMonster and data_type:is_a("app.col_user_data.DamageParamEm") then
             ---@cast char BigEnemy
             box = enemy.big_enemy:new(
-                load_data.col,
+                col,
                 char,
                 load_data.resource_idx,
                 load_data.set_idx,
                 load_data.collidable_idx,
-                load_data.userdata --[[@as app.col_user_data.DamageParamEm]]
+                p_data --[[@as app.col_user_data.DamageParamEm]]
             )
-        elseif char.type == rt.enum.char.SmallMonster then
+        elseif char.type == rt.enum.char.SmallMonster and data_type:is_a("app.col_user_data.DamageParamEm") then
             ---@cast char SmallEnemy
-            box = enemy.small_enemy:new(
-                load_data.col,
-                char,
-                load_data.resource_idx,
-                load_data.set_idx,
-                load_data.collidable_idx
-            )
+            box = enemy.small_enemy:new(col, char, load_data.resource_idx, load_data.set_idx, load_data.collidable_idx)
         end
 
         if box then
             char:add_hurtbox(box)
         end
+        ::continue::
     end
 end
 
